112a15f36e21dd62f3cda2cfabb7a663d01548a3
Jiihyun <cwlgusc@gmail.com>
2023-11-28 16:13:37 +0900

    fix(PrintFormat): 출력형식 오타 수정 -> 테스트 통과

--- src/main/java/lotto/view/constants/PrintFormat.java
+++ src/main/java/lotto/view/constants/PrintFormat.java
@@ -9,7 +9,7 @@ public enum PrintFormat {
             "---" + System.lineSeparator() +
             "3개 일치 (5,000원) - %d개" + System.lineSeparator() +
             "4개 일치 (50,000원) - %d개" + System.lineSeparator() +
-            "5개 일치 (1,500,000원) -%d개" + System.lineSeparator() +
+            "5개 일치 (1,500,000원) - %d개" + System.lineSeparator() +
             "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개" + System.lineSeparator() +
             "6개 일치 (2,000,000,000원) - %d개" + System.lineSeparator() +
             "총 수익률은 %,.1f%%입니다.");
